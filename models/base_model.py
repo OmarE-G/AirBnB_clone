16d5b584e7259688fec30a0f60e7764166694ab6
Omar Essam <omar161157@eng.psu.edu.eg>
2024-03-10 09:48:38 +0200

    New commit

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,8 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+            storage.new(self)
+    
 
     def __str__(self):
         """ str function """
@@ -30,7 +31,6 @@ class BaseModel:
     def save(self):
         """ updates the datetime """
         self.updated_at = datetime.now()
-        storage.new(self)
         storage.save()
 
     def  to_dict(self):
